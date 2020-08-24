b815d5c56efc692118dcdace3f0a6b76a289a02c
Manigandan Krishnakumar <manigandan.ashvin@gmail.com>
2020-08-24 13:21:18 +0530

    Updates

---
+++ precache-manifest.da911ab95043ac52033cb887e043d922.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cba2f71bb22443cad0751d11767bd441",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "45bc0c3caf7df9e69c28",
+    "url": "/portfolio/static/css/main.8dd24bcb.chunk.css"
+  },
+  {
+    "revision": "41acc73d9e290c366435",
+    "url": "/portfolio/static/js/2.f5725a7d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.f5725a7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45bc0c3caf7df9e69c28",
+    "url": "/portfolio/static/js/main.9bdb9d76.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "0495fd2ac8da2244a046ead6c09d1c8a",
+    "url": "/portfolio/static/media/dev-dark.0495fd2a.svg"
+  },
+  {
+    "revision": "7e73afa2e36b0dd7bd32e63126ecc053",
+    "url": "/portfolio/static/media/dev-memoji.7e73afa2.png"
+  },
+  {
+    "revision": "155222f24e1cbcc199a37b7bfe7a6832",
+    "url": "/portfolio/static/media/dev.155222f2.svg"
+  },
+  {
+    "revision": "34ce1459666dbe0f1e6edf969b02d317",
+    "url": "/portfolio/static/media/mani-logo-white.34ce1459.svg"
+  },
+  {
+    "revision": "163823231ed7ec629f00d59f1463c249",
+    "url": "/portfolio/static/media/mani-logo.16382323.svg"
+  },
+  {
+    "revision": "11fd79bcf7943677aa0a7217a8e19470",
+    "url": "/portfolio/static/media/segoeuil.11fd79bc.ttf"
+  }
+]);
